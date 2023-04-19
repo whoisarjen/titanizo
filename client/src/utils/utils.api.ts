be62c3625561e79617d56d49b051eb30edf8f150
Kamil Owczarek <Kamilow97@gmail.com>
2023-04-19 21:42:12 +0200

    env check

--- client/src/utils/utils.api.ts
+++ client/src/utils/utils.api.ts
@@ -1,11 +1,10 @@
-const TOKEN =
-    '***REDACTED***'
+import { env } from '@/env/client.mjs'
 
 export async function getData<GetData>(slug: string) {
     let res = await fetch(`http://127.0.0.1:1337/api${slug}`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer ${TOKEN}`,
+            Authorization: `Bearer ${env.NEXT_PUBLIC_API_TOKEN}`,
         },
         next: {
             revalidate: 10,
