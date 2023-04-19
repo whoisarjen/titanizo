11c980b406495e460bb706479fc07aa2be51132a
Kamil Owczarek <Kamilow97@gmail.com>
2023-04-19 21:42:12 +0200

    env check

---
+++ client/src/env/schema.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { z } from "zod";
+import { env } from "process"
+
+/**
+ * Specify your client-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
+ */
+export const clientSchema = z.object({
+    NEXT_PUBLIC_API_TOKEN: z.string(),
+});
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
+ */
+export const clientEnv = {
+    NEXT_PUBLIC_API_TOKEN: env.API_TOKEN,
+};
