569e521322b4445c224904d09b6b11b99c51a56a
Kamil Owczarek <Kamilow97@gmail.com>
2023-04-21 21:54:20 +0200

    v

--- client/src/app/bag/page.tsx
+++ client/src/app/bag/page.tsx
@@ -1,10 +1,10 @@
-import BagList from './BagList'
+import BagContent from './BagContent'
 import BagSidebar from './BagSidebar'
 
 const Bag = () => {
     return (
         <div className="mx-auto flex w-full max-w-7xl gap-6 p-6">
-            <BagList />
+            <BagContent />
             <BagSidebar />
         </div>
     )
