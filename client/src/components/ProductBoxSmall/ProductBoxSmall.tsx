61c8c6c5f5d154df3b5e47c6b68ee7514fe6e5d5
Kamil Owczarek <Kamilow97@gmail.com>
2023-04-28 23:13:41 +0200

    v

--- client/src/components/ProductBoxSmall/ProductBoxSmall.tsx
+++ client/src/components/ProductBoxSmall/ProductBoxSmall.tsx
@@ -19,7 +19,7 @@ export const ProductBoxSmall = ({
         >
             <div className="relative aspect-square w-3/5">
                 <Image
-                    src={`${env.NEXT_PUBLIC_SERVER_ADDRESS}${product.attributes.images?.data?.[0]?.attributes.formats.small.url || ''}`}
+                    src={`${env.NEXT_PUBLIC_SERVER_ADDRESS}${product.attributes.images?.data?.[0]?.attributes.formats.small?.url || ''}`}
                     alt={product.attributes.name}
                     fill
                     className="object-contain"
