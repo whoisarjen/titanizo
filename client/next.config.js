332a8d8fceaf5244bfeaa89f3ca538a0d381a0c7
Kamil Owczarek <Kamilow97@gmail.com>
2023-05-03 00:38:39 +0200

    v

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      '0.0.0.0',
-      '0.0.0.0:1337',
-      'localhost',
-      'localhost:1337',
-      'media.deante.pl',
-      'titanizo.pl',
-      'titanizo.pl:1337',
-    ]
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
   experimental: {
     appDir: true,
