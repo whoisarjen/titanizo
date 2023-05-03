debffbdb8f971ea5d5599fedefa6ffc59127e95c
Kamil Owczarek <Kamilow97@gmail.com>
2023-05-03 13:27:40 +0200

    font
    v

--- client/src/components/ProductBoxSmall/ProductBoxSmall.tsx
+++ client/src/components/ProductBoxSmall/ProductBoxSmall.tsx
@@ -1,31 +1,37 @@
-import { getProductHref } from '@/utils/product.utils'
+import { formatPrice, getProductHref } from '@/utils/product.utils'
 import Link from 'next/link'
 import { WrappedImage } from '../WrappedImage'
 
 interface ProductBoxSmallProps {
     product: Product
-    className?: string
 }
 
-export const ProductBoxSmall = ({
-    product,
-    className,
-}: ProductBoxSmallProps) => {
+export const ProductBoxSmall = ({ product }: ProductBoxSmallProps) => {
     return (
         <Link
             href={getProductHref(product)}
-            className={`relative flex w-full max-w-md flex-col items-center gap-4 p-4 shadow md:max-w-sm ${className}`}
+            className="flex w-full flex-col gap-2 text-sm"
         >
-            <div className="relative aspect-square w-3/5">
+            <div className="relative aspect-square w-full rounded-lg shadow">
                 <WrappedImage
-                    src={product.attributes.images?.data?.[0]?.attributes.formats.small?.url || ''}
+                    src={
+                        product.attributes.images?.data?.[0]?.attributes.formats
+                            .small?.url || ''
+                    }
                     alt={product.attributes.name}
                     fill
-                    className="object-contain"
-                    loading='lazy'
+                    className="object-contain p-8"
+                    loading="lazy"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
                 />
             </div>
-            <h2 className="block font-bold">{product.attributes.name}</h2>
+            <h2 className="truncate text-base font-bold">
+                {product.attributes.name}
+            </h2>
+            <div className="line-clamp-2">{product.attributes.description}</div>
+            <div className="font-bold">
+                {formatPrice(product.attributes.gross_price)}
+            </div>
         </Link>
     )
 }
