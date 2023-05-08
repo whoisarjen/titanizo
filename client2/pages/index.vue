ce88c8c7787c8356df5dce170c56f1e67f35aab6
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2023-05-08 04:06:20 +0200

    updaet

--- client2/pages/index.vue
+++ client2/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-    Strona główna
+    <section class="w-full md:w-[70vw] mx-auto h-[600px] bg-slate-600 flex justify-center items-center text-3xl text-white">
+        Bannerki
+    </section>
+    <SalesSelectedForYou />
+    <SalesPopularCategories />
+
+    <section class="container mx-auto h-[600px] bg-orange-600 flex justify-center items-center text-3xl text-white">
+       Inspiracje
+    </section>
+
+    <BlogLatestPosts />
+    <SalesLastWatched />
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: "Najlepszy sklep internetowy"
+})
+</script>
