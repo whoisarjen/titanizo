a945ac5ba48327995ba811fd990e01e45d750bfc
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2023-05-10 02:38:07 +0200

    update(ProductCard + Sales): now uses Product objects

--- client/components/Product/Card/index.vue
+++ client/components/Product/Card/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <article>
-        <NuxtLink class="group">
-            <nuxt-img src="https://media.deante.pl/decor/BATERIE/Baterie%20%C5%82azienkowe/ARNIKA_BQA/BQA_D21K.jpg" fit="contain" class="w-2/3 mx-auto mix-blend-darken py-8"/>
-            <h5 class="text-xl uppercase tracking-tighter font-medium line-clamp-1 mb-1 group-hover:underline">Deante Arnika Bateria umywalkowa wysoka</h5>
-            <h6 class="tracking-tight line-clamp-2">Bateria umywalkowa wysoka</h6>
-            <ProductPrice :price="699.99" :promo-price="549.99"/>
-            <span class="text-xs tracking-tight mt-2 leading-snug block">Najniższa cena z ostatnich 30 dni przed wprowadzeniem obniżki: <b>569,99 zł</b></span>
+    <article v-if="product">
+        <NuxtLink class="group flex flex-col h-full" :to="`/produkt/${product.id}--${slugify(product.attributes.name, { lower: true })}`">
+            <nuxt-img v-if="product.attributes.images?.data?.length" :src="`https://strapi.titanizo.pl${product.attributes.images?.data[0].attributes.formats.small.url}`" class="w-4/5 aspect-square object-contain mx-auto mix-blend-darken py-8"/>
+            <h5 class="text-xl uppercase tracking-tighter font-medium line-clamp-1 mb-1 group-hover:underline break-all">{{ product.attributes.manufacturer.data.attributes.name }} {{ product.attributes.collection }} {{ product.attributes.name }} </h5>
+            <h6 class="tracking-tight line-clamp-2 flex-grow mb-2">{{ product.attributes.name }}</h6>
+            <ProductPrice :price="product.attributes.gross_price"/>
+            <!-- <span class="text-xs tracking-tight mt-2 leading-snug block">Najniższa cena z ostatnich 30 dni przed wprowadzeniem obniżki: <b>{{ }} zł</b></span> -->
             <div class="invisible group-hover:visible block py-4 uppercase text-white bg-sky-600 mt-4 rounded-full text-center font-medium cursor-pointer hover:bg-sky-800 transition-colors">
                 Do koszyka
                 <font-awesome-icon size="xl" class="ml-2" icon="fa-light fa-bag-shopping" />
@@ -13,3 +13,12 @@
         </NuxtLink>
     </article>
 </template>
+
+<script setup lang="ts">
+import slugify from 'slugify';
+
+const { product } = defineProps<{
+    product: Product
+}>()
+
+</script>
--- client/components/Sales/LastWatched/index.vue
+++ client/components/Sales/LastWatched/index.vue
@@ -1,8 +1,29 @@
 <template>
     <section class="container mx-auto mb-16">
         <h2 class="heading text-center">Ostatnio przeglądane</h2>
-        <div class="grid grid-cols-4 mt-4 gap-20">
-            <ProductCard v-for="i in 2" :key="i"/>
+        <div class="grid grid-cols-4 mt-4 gap-20 overflow-auto max-w-screen">
+            <ProductCard v-for="product in products?.data" :key="product.id" :product="product" />
         </div>
     </section>
 </template>
+
+
+<script setup lang="ts">
+type GetProduct = {
+    data: Product[]
+    meta: Meta
+}
+
+const { data: products } = await useGetApi<GetProduct>(`/products`, {
+    'populate[0]': 'images',
+    'populate[1]': 'manufacturer',
+    'populate[2]': 'subcategories.category',
+    'populate[3]': 'recommended_products.images',
+    'populate[4]': 'recommended_products.subcategories.category',
+    'pagination[page]': '1',
+    'pagination[pageSize]': '2',
+    'sort': 'name:desc'
+})
+
+
+</script>
