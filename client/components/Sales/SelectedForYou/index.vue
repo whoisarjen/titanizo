501aca7f38d6de98603730e4d3de049a1067c32b
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2023-05-10 02:38:07 +0200

    update(ProductCard + Sales): now uses Product objects

--- client/components/Sales/SelectedForYou/index.vue
+++ client/components/Sales/SelectedForYou/index.vue
@@ -1,8 +1,35 @@
 <template>
     <section class="container mx-auto mb-16">
         <h2 class="heading">Wybrane dla Ciebie</h2>
-        <div class="grid grid-cols-4 mt-4 gap-20 overflow-auto max-w-screen">
-            <ProductCard v-for="i in 4" :key="i" />
+        <div class="grid mt-4 gap-20 overflow-auto max-w-screen" :class="`grid-cols-${gridSize}`">
+            <ProductCard v-for="product in products?.data" :key="product.id" :product="product" />
         </div>
     </section>
 </template>
+
+
+<script setup lang="ts">
+const { gridSize } = withDefaults(defineProps<{
+    gridSize: number
+}>(), {
+    gridSize: 4
+})
+
+type GetProduct = {
+    data: Product[]
+    meta: Meta
+}
+
+const { data: products } = await useGetApi<GetProduct>(`/products`, {
+    'populate[0]': 'images',
+    'populate[1]': 'manufacturer',
+    'populate[2]': 'subcategories.category',
+    'populate[3]': 'recommended_products.images',
+    'populate[4]': 'recommended_products.subcategories.category',
+    'pagination[page]': '1',
+    'pagination[pageSize]': `${gridSize}`,
+    'sort': 'gross_price:desc'
+})
+
+
+</script>
