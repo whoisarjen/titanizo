ac678228291a9ace3c6b778e74e8dc20675a034e
Kamil Owczarek <Kamilow97@gmail.com>
2023-05-11 21:31:18 +0200

    Update cron.ts

--- server/config/cron.ts
+++ server/config/cron.ts
@@ -512,7 +512,7 @@ export default {
       console.log("Finished synchronization with Deante!");
     },
     options: {
-      rule: "0 30 23 * * *",
+      rule: "0 48 19 * * *",
     },
   },
 };
