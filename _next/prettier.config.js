f10538de44ef0547413a09697aa769ba25e8ef15
Kamil Owczarek <Kamilow97@gmail.com>
2023-05-11 23:33:10 +0200

    remove next + call for categories

--- _next/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [require('prettier-plugin-tailwindcss')],
-    tailwindConfig: './tailwind.config.js',
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-}
