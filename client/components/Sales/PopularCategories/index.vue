43317430730bff2784bfb4813534bd59040921ad
Kamil Owczarek <Kamilow97@gmail.com>
2023-05-21 17:12:53 +0200

    Update index.vue

--- client/components/Sales/PopularCategories/index.vue
+++ client/components/Sales/PopularCategories/index.vue
@@ -3,16 +3,16 @@
     <h2 class="heading">
       Popularne kategorie
     </h2>
-    <nuxt-link class="grid grid-cols-5 mt-4 gap-10 max-w-screen overflow-auto" to="/#">
-      <div v-for="category in categories?.data" :key="category.id" class="group cursor-pointer">
+    <div class="grid grid-cols-5 mt-4 gap-10 max-w-screen overflow-auto">
+      <nuxt-link v-for="category in categories?.data" :key="category.id" class="group cursor-pointer" to="/#">
         <div class="h-[300px]  relative overflow-hidden">
           <nuxt-img loading="lazy" :src="`https://strapi.titanizo.pl${category.attributes.image?.data?.attributes.formats.small.url}`" class="object-cover w-full h-full group-hover:scale-125 transition-transform duration-700" />
         </div>
         <h6 class="text-xl bottom-0 uppercase font-medium mt-2 tracking-tighter">
           {{ category.attributes.name }}
         </h6>
-      </div>
-    </nuxt-link>
+      </nuxt-link>
+    </div>
   </section>
 </template>
 
